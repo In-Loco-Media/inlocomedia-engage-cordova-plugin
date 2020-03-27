8e9f52987767f980b4ca4b9175deefb30671225d
Douglas Soares <douglas.soares@inloco.com.br>
2020-03-27 17:51:51 -0300

    feat(InLocoEngagePlugin, InLocoEngage): exposes sdk public methods

--- www/InLocoEngage.js
+++ www/InLocoEngage.js
@@ -4,21 +4,55 @@ var inLocoEngageExport = {};
 
 inLocoEngageExport.OPTIONS = {
   // setUser options
-  USER_ID: 'userId',
+  USER_ID: 'user_id',
 
   // trackEvent options
   EVENT_NAME: 'name',
   EVENT_PROPERTIES: 'properties',
 
+  // checkIn options
+  PLACE_NAME: 'place_name',
+  PLACE_ID: 'place_id',
+  EXTRAS: 'extras',
+
+  // addressValidation options
+  LANGUAGE: 'language',
+  COUNTRY: 'country',
+  COUNTRY_NAME: 'country_name',
+  COUNTRY_CODE: 'country_code',
+  ADMIN_AREA: 'admin_area',
+  SUB_ADMIN_AREA: 'sub_admin_area',
+  LOCALITY: 'locality',
+  SUB_LOCALITY: 'sub_locality',
+  THOROUGHFARE: 'thoroughfare',
+  SUB_THOROUGHFARE: 'sub_thoroughfare',
+  POSTAL_CODE: 'postal_code',
+  LATITUDE: 'latitude',
+  LONGITUDE: 'longitude',
+
   // givePrivacyConsent options
-  CONSENT_STATE: 'consent'
+  CONSENT: 'consent',
+  CONSENT_TYPES: 'consent_types',
+
+  // consentTypes options
+  ADDRESS_VALIDATION: "address_validation",
+  ADVERTISEMENT: "advertisement",
+  ENGAGE: "engage",
+  EVENTS: "analytics",
+  INSTALLED_APPS: "installed_apps",
+  LOCATION: "location",
+  CONTEXT_PROVIDER: "context_provider"
 };
 
 inLocoEngageExport.ACTIONS = {
   SET_USER: 'setUser',
   CLEAR_USER: 'clearUser',
   TRACK_EVENT: 'trackEvent',
-  GIVE_PRIVACY_CONSENT: 'givePrivacyConsent'
+  REGISTER_CHECK_IN: 'registerCheckIn',
+  SET_ADDRESS: 'setAddress',
+  CLEAR_ADDRESS: 'clearAddress',
+  GIVE_PRIVACY_CONSENT: 'givePrivacyConsent',
+  CHECK_PRIVACY_CONSENT_MISSING: 'checkPrivacyConsentMissing'
 };
 
 inLocoEngageExport.setUser = function(args, successCallback, failureCallback) {
@@ -33,8 +67,24 @@ inLocoEngageExport.trackEvent = function(args, successCallback, failureCallback)
   cordova.exec(successCallback, failureCallback, 'InLocoEngage', inLocoEngageExport.ACTIONS.TRACK_EVENT, [args]);
 };
 
+inLocoEngageExport.registerCheckIn = function(args, successCallback, failureCallback) {
+  cordova.exec(successCallback, failureCallback, 'InLocoEngage', inLocoEngageExport.ACTIONS.REGISTER_CHECK_IN, [args]);
+};
+
+inLocoEngageExport.setAddress = function(args, successCallback, failureCallback) {
+  cordova.exec(successCallback, failureCallback, 'InLocoEngage', inLocoEngageExport.ACTIONS.SET_ADDRESS, [args]);
+};
+
+inLocoEngageExport.clearAddress = function(successCallback, failureCallback) {
+  cordova.exec(successCallback, failureCallback, 'InLocoEngage', inLocoEngageExport.ACTIONS.CLEAR_ADDRESS, []);
+}
+
 inLocoEngageExport.givePrivacyConsent = function(args, successCallback, failureCallback) {
   cordova.exec(successCallback, failureCallback, 'InLocoEngage', inLocoEngageExport.ACTIONS.GIVE_PRIVACY_CONSENT, [args]);
+};
+
+inLocoEngageExport.checkPrivacyConsentMissing = function(successCallback, failureCallback) {
+  cordova.exec(successCallback, failureCallback, 'InLocoEngage', inLocoEngageExport.ACTIONS.CHECK_PRIVACY_CONSENT_MISSING, []);
 };
 
 module.exports = inLocoEngageExport;
